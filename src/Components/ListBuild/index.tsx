12189fb79121c9944f522f2df95d4e83174e2d37
Vladimir <volody.grab@yandex.ru>
2020-07-13 00:21:44 +0500

    update from 110720

--- src/Components/ListBuild/index.tsx
+++ src/Components/ListBuild/index.tsx
@@ -14,25 +14,41 @@ export default class ListBuild extends React.Component<PropsListBuild, {}>{
     }
 
 
+    build_tree_Recurs=(arrayList:Array<any>, parent_id:number)=>{
+        console.log(arrayList)
+        return(
+            <div>
+                {
+                   isArray(arrayList) && (typeof(arrayList[parent_id]) != "undefined" && arrayList[parent_id] !== null) ?
+                       <ul  className='treeCSS'>
+                       {arrayList[0][parent_id].map((val:any, index:number)=>(
+                               <li><span className='build_name' data-idbuild={val.build_ID} data-idparent={val.build_PARENT_ID}><span id='title_build'>{val.build_name}</span></span>
+                               {this.build_tree_Recurs(arrayList, val.build_ID) }
+                               </li>
+                       ))}
+                      
+                           </ul>
+                    : ""
+                }   
+            </div>
+        )
+    }
+
 
 
     build_tree(arrayList:Array<any>, parent_id:number){
         let tree = ""
         if( isArray(arrayList) && (typeof(arrayList[parent_id]) != "undefined" && arrayList[parent_id] !== null) ){ 
             tree += "<ul  class='treeCSS'>";
-            for(let val in arrayList){
-                    // tree += "<li><span class='build_name' data-idbuild='"+val.build_ID+"' data-idparent='"+val.build_PARENT_ID+"'><span id='title_build'>"+val.build_name+"</span></span>";
+            arrayList[0][parent_id].map((val:any, index:number)=>{
+                    tree += "<li><span class='build_name' data-idbuild='"+val.build_ID+"' data-idparent='"+val.build_PARENT_ID+"'><span id='title_build'>"+val.build_name+"</span></span>";
                     
-                    // tree += this.build_tree(arrayList, val.build_ID);  
-                    // tree += "</li>";
-                   
-                        console.log(val)
-                   
-                }
+                    tree += this.build_tree(arrayList, val.build_ID);  
+                    tree += "</li>";
+            })
            
                 tree +="</ul>";
         }
-        return tree;
     }
 
 
@@ -42,8 +58,7 @@ export default class ListBuild extends React.Component<PropsListBuild, {}>{
         return(
             <div>
             <div></div>
-                
-                {this.build_tree(this.props.arrayListBuild, 0)}
+                {this.build_tree_Recurs(this.props.arrayListBuild, 0)}
             </div>
         )
     }
